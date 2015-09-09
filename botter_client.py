a71b9906c9ada28379b26f43c7cccc8aed095a66
Peter van Heusden <pvh@sanbi.ac.za>
2015-09-09 15:57:17 +0200

    * Provide sample .ini file * Store state in JSON encoded state file between runs

--- botter_client.py
+++ botter_client.py
@@ -11,30 +11,46 @@ config.read(['botter_sample.ini',
 
 global_config = 'global'
 global_key_option = 'key'
+global_state_option = 'statefile'
 
 if config.has_option(global_config, global_key_option):
     key = config.get(global_config, global_key_option)
 else:
     sys.exit("Need an API key")
 
-BASE_URL = 'https://api.telegram.org/bot{}/'.format(key)
+config.base_url = 'https://api.telegram.org/bot{}/'.format(key)
 
 
-def get_me():
-    response = requests.get(BASE_URL + 'getMe')
+def get_me(config):
+    response = requests.get(config.base_url + 'getMe')
     if response.status_code == 200:
         print(response.text)
     else:
         sys.exit(response)
 
 
-def get_and_reply():
-    response = requests.get(BASE_URL + 'getUpdates')
+def get_and_reply(config):
+    if config.has_option(global_config, global_state_option):
+        statefile_filename = config.get(global_config, global_state_option)
+    else:
+        statefile_filename = 'state.json'
+
+    try:
+        input_file = open(statefile_filename, 'rb')
+    except IOError:
+        state = dict()
+    else:
+        state = json.load(input_file)
+
+    next_message_id = state.get('last_message_id', 0) + 1
+    request_params = dict(offset=str(next_message_id))
+    response = requests.get(config.base_url + 'getUpdates', params=request_params)
     if response.status_code == 200:
         dict_repr = json.loads(response.text)
         results = dict_repr['result']
         for result in results:
             update_id = result['update_id']
+            last_message_id = update_id
             message = result['message']
             if 'text' in message:
                 # we're dealing with a text messages
@@ -43,12 +59,16 @@ def get_and_reply():
                 response = dict(chat_id=user['id'],
                                 text='Hello {}'.format(user['first_name']))
                 print(json.dumps(response))
-                outcome = requests.post(BASE_URL + 'sendMessage', data=response)
+                outcome = requests.post(config.base_url + 'sendMessage', data=response)
                 if outcome.status_code == 200:
                     print("Yay, message sent")
                 else:
                     print(outcome, outcome.text)
+            state['last_message_id'] = last_message_id
+        with open(statefile_filename, 'wb') as output_file:
+            json.dump(state, output_file)
     else:
         sys.exit(response)
+
 if __name__ == '__main__':
-    get_and_reply()
+    get_and_reply(config)
