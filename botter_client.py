f7ff07f23686e062cf751dd12481cfb6d4c4382a
Peter van Heusden <pvh@sanbi.ac.za>
2015-09-09 14:03:29 +0200

    * Working botter 0.2

--- botter_client.py
+++ botter_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import json
 from ConfigParser import SafeConfigParser
 import requests
 
@@ -26,5 +27,28 @@ def get_me():
     else:
         sys.exit(response)
 
+
+def get_and_reply():
+    response = requests.get(BASE_URL + 'getUpdates')
+    if response.status_code == 200:
+        dict_repr = json.loads(response.text)
+        results = dict_repr['result']
+        for result in results:
+            update_id = result['update_id']
+            message = result['message']
+            if 'text' in message:
+                # we're dealing with a text messages
+                user = message['from']
+                print("from:", user, "text", message['text'])
+                response = dict(chat_id=user['id'],
+                                text='Hello {}'.format(user['first_name']))
+                print(json.dumps(response))
+                outcome = requests.post(BASE_URL + 'sendMessage', data=response)
+                if outcome.status_code == 200:
+                    print("Yay, message sent")
+                else:
+                    print(outcome, outcome.text)
+    else:
+        sys.exit(response)
 if __name__ == '__main__':
-    get_me()
+    get_and_reply()
